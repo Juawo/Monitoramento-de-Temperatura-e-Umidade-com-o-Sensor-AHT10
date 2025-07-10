1395edfe5ac7d010b2d916ccaac87aabf31c553f
Juawo <joaopedrojp2703@gmail.com>
2025-07-10 13:00:36 -0300

    Adicionando buzzer

---
+++ read-temp-humi/include/buzzer.h
@@ -0,0 +1,14 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+
+#define BUZZER_PIN 21
+#define BUZZER_FREQUENCY 100
+
+void pwm_init_buzzer(uint pin);
+void beep(uint pin, uint duration_ms);
+
+#endif
--- read-temp-humi/read-temp-humi.c
+++ read-temp-humi/read-temp-humi.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
-
-
+#include "include/temp_humi_sensor.h"
+#include "include/buzzer.h"
 
 int main()
 {
     stdio_init_all();
 
     // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
+    i2c_init(I2C_PORT, 400 * 1000);
+    pwm_init_buzzer(BUZZER_PIN);
+
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
+
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    while (true) {
-        printf("Hello, world!\n");
+    if (!aht10_init())
+    {
+        printf("Falha ao inicializar o sensor AHT10!\n");
+        return 1;
+    }
+
+    while (true)
+    {
+        float temp, hum;
+        if (aht10_read(&temp, &hum))
+        {
+            printf("Temperatura : %.2f C | Umidade : %.2f %%\n", temp, hum);
+            if (temp < 20 || hum > 70)
+            {
+                printf("beep\n");
+                beep(BUZZER_PIN, 500);
+            }
+        }
+        else
+        {
+            printf("Erro na leitura dos dados!\n");
+        }
+
         sleep_ms(1000);
     }
 }
